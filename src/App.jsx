5f3ecd1c9478137920bdd04268f1fffbfb8b35e1
kirtymeena <kirtymeena@gmail.com>
2023-08-05 13:58:23 +0530

    removed commented code

--- src/App.jsx
+++ src/App.jsx
@@ -14,21 +14,6 @@ function App() {
   const [limit, setLimit] = useState(30)
   const [totalContent, setTotalContent] = useState(0)
 
-  // const newsApi = async () => {
-  //   try {
-  //     const news = await axios.get(`http://api.mediastack.com/v1/news?access_key=${API_KEY}&limit=${limit}&sort=published_desc& &categories=${category.toLowerCase()}`, {
-  //       languages: 'fr,-en',
-  //       countries: 'in,us',
-  //     })
-
-  //     console.log(news.data)
-  //     setNewsResults(news.data.data)
-  //     setTotalContent(news.data.pagination.total)
-  //   } catch (err) {
-  //     console.log(err)
-
-  //   }
-  // }
   const newsApi = async () => {
     try {
       const news = await axios.get(`https://api.nytimes.com/svc/news/v3/content/nyt/${category}.json/?limit=${limit}&api-key=${API_KEY}`)
